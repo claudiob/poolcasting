7fd13c725e359bbe86c6616ce69aead2b4b00f92
Claudio B <claudiob@Claudiob.iiia.csic.es>
2009-03-13 17:45:52 +0100

    With Rails 2.3, controllers/application.rb has been renamed application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '14059614cdee056d6f7968abd9a99316'
-  
+
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
